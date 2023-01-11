ef0c30d2d58770b180c7125130e3da7a31d8c4f2
boubelpi <113375173+boubelpi@users.noreply.github.com>
2023-01-11 12:01:25 +0100

    fixed bug in scroll view

--- Yuri-Yurika/scroll.swift
+++ Yuri-Yurika/scroll.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct scroll: View {
     var body: some View {
         ScrollView(.horizontal,showsIndicators : false) {
-            ForEach(0..<10) {
-                Text("Item \($0)")
-                    .foregroundColor(.white)
-                    .background(.red)
-                    .font(.largeTitle)
-                    .frame(width : 200, height : 200)
+            HStack {
+                ForEach(0..<10) {
+                    Text("Item \($0)")
+                        .foregroundColor(.white)
+                        .background(.red)
+                        .font(.largeTitle)
+                        .frame(width : 200, height : 200)
+                }
             }
         }
         .frame(height:350)
