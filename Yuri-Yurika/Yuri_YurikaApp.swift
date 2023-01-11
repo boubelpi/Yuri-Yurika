2d60a521df0b232d89f18a117520a6e9050e6461
boubelpi <113375173+boubelpi@users.noreply.github.com>
2023-01-11 11:05:33 +0100

    Initial Commit

---
+++ Yuri-Yurika/Yuri_YurikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yuri_YurikaApp.swift
+//  Yuri-Yurika
+//
+//  Created by Pavel Boubel on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Yuri_YurikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
